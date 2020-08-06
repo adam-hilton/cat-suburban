033e0bee5e6258e5a4f9c508f9067f11cb741ab1
adam.hilton <adam.hilton@accenture.com>
2020-08-06 13:45:41 -0500

    loaded test game

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ const prodRealURL = process.env.prodRealURL;
 app.webhookURL = prodRealURL;
 
 // Set questions
-app.qs = require('./questions/qs-ssc.js');
+app.qs = require('./questions/qs-5.js');
 
 
 gameInit(app);
