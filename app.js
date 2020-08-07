c63f8df3b3f9c68c238cbab9161dc88f534a0b79
adam.hilton <adam.hilton@accenture.com>
2020-08-06 20:41:49 -0500

    loading new game

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ const prodRealURL = process.env.prodRealURL;
 app.webhookURL = prodRealURL;
 
 // Set questions
-app.qs = require('./questions/qs-phillip.js');
+app.qs = require('./questions/qs-taylor.js');
 
 
 gameInit(app);
