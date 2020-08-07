103cb65931d3d05350eb85f3a78f0bb59264a121
adam.hilton <adam.hilton@accenture.com>
2020-08-06 21:01:42 -0500

    loading new game

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ const prodRealURL = process.env.prodRealURL;
 app.webhookURL = prodRealURL;
 
 // Set questions
-app.qs = require('./questions/qs-lauren-2.js');
+app.qs = require('./questions/qs-tyler-dune.js.js');
 
 
 gameInit(app);
